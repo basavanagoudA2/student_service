e88628514db376186f0d195ec5300b67979304e2
Basavanagoud A <bsgoud78900@gmail.com>
2023-08-29 18:21:00 +0530

    added data variable

--- src/main/java/com/bm/world/controller/StudentController.java
+++ src/main/java/com/bm/world/controller/StudentController.java
@@ -37,6 +37,7 @@ public class StudentController {
     @PostMapping(value = ApplicationConstants.STUDENT_SAVE,consumes = "application/json")
     public ResponseEntity<SaveResponse> saveStudent(@RequestBody @Valid StudentRequest studentRequest){
         Date date=new Date();
+        String data="";
         SaveResponse saveResponse=studentService.saveStudent(studentRequest);
         return new ResponseEntity<>(saveResponse, HttpStatus.OK);
     }

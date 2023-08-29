e0bd46200c833b876d008f56764b1112487705a2
Basavanagoud A <bsgoud78900@gmail.com>
2023-08-29 18:09:10 +0530

    added comments on controller

--- src/main/java/com/bm/world/controller/StudentController.java
+++ src/main/java/com/bm/world/controller/StudentController.java
@@ -15,6 +15,9 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+/**
+ * ysdfjpodfp
+ */
 @RestController
 @RequestMapping(value = ApplicationConstants.BASE_URL)
 public class StudentController {
